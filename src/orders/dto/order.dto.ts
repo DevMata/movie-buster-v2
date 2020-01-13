a464d18387de1d93925566aac49e603b90034040
Antonio Mata <hildeamp@gmail.com>
2020-01-12 21:13:36 -0600

    :recycle: refactor order dto

--- src/orders/dto/order.dto.ts
+++ src/orders/dto/order.dto.ts
@@ -1,6 +1,11 @@
-import { IsUUID } from 'class-validator';
+import { IsUUID, IsOptional, ValidateNested } from 'class-validator';
+import { SubOrderInfo } from 'src/order-details/dto/order-info.dto';
 
 export class OrderDto {
   @IsUUID()
   movieId: string;
+
+  @IsOptional()
+  @ValidateNested()
+  order!: SubOrderInfo[];
 }
