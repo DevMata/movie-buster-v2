12b5d0fa4b9c8df8571ee8454b10f49782274b7f
Antonio Mata <hildeamp@gmail.com>
2020-01-13 15:36:52 -0600

    :recycle: export Typeorm and RentDetails service

--- src/rent-details/rent-details.module.ts
+++ src/rent-details/rent-details.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RentDetailsService } from './rent-details.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RentDetails } from './entities/rent-detail.entity';
+import { MovieRepository } from '../movies/repositories/movie.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RentDetails, MovieRepository])],
   providers: [RentDetailsService],
+  exports: [TypeOrmModule, RentDetailsService],
 })
 export class RentDetailsModule {}
