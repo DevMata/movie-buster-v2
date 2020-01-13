243a0518b0e5beeecdeb67febc6091b5f9a1d5f6
Antonio Mata <hildeamp@gmail.com>
2020-01-13 15:55:24 -0600

    update rent dto

--- src/rents/dto/rent.dto.ts
+++ src/rents/dto/rent.dto.ts
@@ -1,6 +1,20 @@
-import { IsUUID } from 'class-validator';
+import {
+  IsUUID,
+  IsOptional,
+  ValidateNested,
+  ArrayNotEmpty,
+} from 'class-validator';
+import { SubOrderInfo } from 'src/order-details/dto/order-info.dto';
+import { Type } from 'class-transformer';
 
 export class RentDto {
   @IsUUID()
+  @IsOptional()
   movieId: string;
+
+  @IsOptional()
+  @ValidateNested()
+  @ArrayNotEmpty()
+  @Type(() => SubOrderInfo)
+  rent: SubOrderInfo[];
 }
