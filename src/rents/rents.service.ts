713a87e758dd5d05254193b77df4c123c962b5ab
Antonio Mata <hildeamp@gmail.com>
2020-01-13 15:55:47 -0600

    :sparkles: add makeRent

--- src/rents/rents.service.ts
+++ src/rents/rents.service.ts
@@ -3,12 +3,17 @@ import {
   NotFoundException,
   UnprocessableEntityException,
   MethodNotAllowedException,
+  BadRequestException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Rent } from './entities/rent.entity';
 import { Repository, UpdateResult } from 'typeorm';
-import { UserRepository } from 'src/users/repositories/user.repository';
-import { MovieRepository } from 'src/movies/repositories/movie.repository';
+import { UserRepository } from '../users/repositories/user.repository';
+import { MovieRepository } from '../movies/repositories/movie.repository';
+import { SubOrderInfo } from '../order-details/dto/order-info.dto';
+import { RentDetailsService } from '../rent-details/rent-details.service';
+import { Movie } from '../movies/entities/movie.entity';
+import { RentDetails } from '../rent-details/entities/rent-detail.entity';
 
 @Injectable()
 export class RentsService {
@@ -16,7 +21,51 @@ export class RentsService {
     @InjectRepository(Rent) private readonly rentRepository: Repository<Rent>,
     private readonly userRepository: UserRepository,
     private readonly movieRepository: MovieRepository,
+    private readonly rentDetailsService: RentDetailsService,
   ) {}
+
+  async makeRent(userId: string, rent: Array<SubOrderInfo>): Promise<Rent> {
+    const res = rent
+      .map(suborder => suborder.movieId)
+      .every((id, i, a) => a.indexOf(id) === a.lastIndexOf(id));
+
+    if (!res) {
+      throw new BadRequestException('every movie Id must be unique');
+    }
+
+    const user = await this.userRepository.findOne(userId);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const movies = new Array<Movie>();
+
+    for (const subrent of rent) {
+      const res = await this.rentDetailsService.validateSubRent(subrent);
+      movies.push(res);
+    }
+
+    const subrentsInfo = movies.map((movie, i) => ({
+      movie,
+      quantity: rent[i].quantity,
+    }));
+
+    const subrents = new Array<RentDetails>();
+
+    for (const subrent of subrentsInfo) {
+      const res = await this.rentDetailsService.makeSubRent(
+        subrent.movie,
+        subrent.quantity,
+      );
+      subrents.push(res);
+    }
+
+    const total = subrents
+      .map(suborder => suborder.subTotal)
+      .reduce((a, b) => a + b);
+
+    return this.rentRepository.save({ user, details: subrents, total });
+  }
 
   async rentMovie(
     userId: string,
@@ -57,10 +106,10 @@ export class RentsService {
       );
     }
 
-    const stock = (await this.movieRepository.findOne(rent.movie.movieId))
-      .stock;
+    // const stock = (await this.movieRepository.findOne(rent.movie.movieId))
+    //   .stock;
 
-    this.movieRepository.update(rent.movie.movieId, { stock: stock + 1 });
+    // this.movieRepository.update(rent.movie.movieId, { stock: stock + 1 });
     return this.rentRepository.update(rent.rentId, { status: 'returned' });
   }
 }
