df65ccb99a62024a8ae3d5a29affb8ec6cee04d4
Antonio Mata <hildeamp@gmail.com>
2020-01-13 16:07:51 -0600

    :recycle: update rent dto

--- src/rents/dto/rent.dto.ts
+++ src/rents/dto/rent.dto.ts
@@ -1,18 +1,8 @@
-import {
-  IsUUID,
-  IsOptional,
-  ValidateNested,
-  ArrayNotEmpty,
-} from 'class-validator';
+import { ValidateNested, ArrayNotEmpty } from 'class-validator';
 import { SubOrderInfo } from 'src/order-details/dto/order-info.dto';
 import { Type } from 'class-transformer';
 
 export class RentDto {
-  @IsUUID()
-  @IsOptional()
-  movieId: string;
-
-  @IsOptional()
   @ValidateNested()
   @ArrayNotEmpty()
   @Type(() => SubOrderInfo)
