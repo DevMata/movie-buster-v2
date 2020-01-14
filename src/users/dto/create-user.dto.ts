48dce8795e8c5bc8601eaa9b4b77b2698565d6fe
Antonio Mata <hildeamp@gmail.com>
2020-01-14 14:34:01 -0600

    decorate with ApiProperty

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,8 @@
 import { IsString, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
