04ebf71b0bc899f54dba7a39f4e2bca6b0a55ac9
Antonio Mata <hildeamp@gmail.com>
2020-01-13 18:55:53 -0600

    implement send method

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { setApiKey, send } from '@sendgrid/mail';
+import { User } from '../users/entities/user.entity';
+import { Rent } from '../rents/entities/rent.entity';
+import { Order } from '../orders/entities/order.entity';
 
 @Injectable()
 export class EmailService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {
+    setApiKey(this.configService.get('SENDGRID_API_KEY'));
+  }
 
   setApiKey(): void {
     setApiKey(this.configService.get('SENDGRID_API_KEY'));
   }
 
-  send(): void {
+  send(user: User, order: Order | Rent): void {
     const msg = {
-      to: 'test@example.com',
+      to: user.email,
       from: 'test@example.com',
       subject: 'Sending with Twilio SendGrid is Fun',
       text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      html: JSON.stringify(order),
     };
     send(msg);
   }
