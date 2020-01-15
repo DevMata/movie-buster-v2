fb835ce62700d43897ed02fae31898ecb4f3bb21
Antonio Mata <hildeamp@gmail.com>
2020-01-15 11:54:27 -0600

    :sparkles: MeController

---
+++ src/users/me.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { LoggedUser } from './decorators/user.decorator';
+import { UserPayload } from 'src/authentication/dto/user-payload.dto';
+
+@Controller('users/me')
+export class MeController {
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  getProfile(@LoggedUser() user: UserPayload): void {
+    console.log(user);
+  }
+}
