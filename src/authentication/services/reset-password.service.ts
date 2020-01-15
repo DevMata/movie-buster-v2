679af8499cdd9f7ffd4edd337b8ffad910b6b77a
Antonio Mata <hildeamp@gmail.com>
2020-01-15 17:20:53 -0600

    :sparkles: Add logic for create a reset token and send a email for user

--- src/authentication/services/reset-password.service.ts
+++ src/authentication/services/reset-password.service.ts
@@ -1,15 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { UserRepository } from '../../users/repositories/user.repository';
+import { EmailService } from '../../email/email.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AccessToken } from '../entities/token.entity';
+import { Repository } from 'typeorm';
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+import { sign } from 'jsonwebtoken';
 
 @Injectable()
 export class ResetPasswordService {
   constructor(
     private readonly configService: ConfigService,
     private readonly userRepository: UserRepository,
+    private readonly emailService: EmailService,
+    @InjectRepository(AccessToken)
+    private readonly accessTokenRepository: Repository<AccessToken>,
   ) {}
 
-  sendResetEmail(): void {
-    console.log('');
+  async sendResetEmail(email: string): Promise<void> {
+    const user = await this.userRepository.findUserByEmail(email);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const secret = this.configService.get('JWT_SECRET_RESET_PASSWORD');
+    const jti = randomStringGenerator();
+    const payload = { sub: user.userId, email: user.email };
+
+    await this.accessTokenRepository.save({ userId: user.userId, jti });
+
+    const token = sign(payload, secret, { jwtid: jti, expiresIn: '6h' });
+
+    await this.emailService.sendResetPasswordEmail(user.email, token);
   }
 }
