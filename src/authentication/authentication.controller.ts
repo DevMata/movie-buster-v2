0c2b74cf980250474823b92672d278ed545f836e
Antonio Mata <hildeamp@gmail.com>
2020-01-14 18:13:44 -0600

    decorate with ApiBody

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -3,13 +3,15 @@ import { AuthenticationService } from './services/authentication.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { DeleteResult } from 'typeorm';
-import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags, ApiBearerAuth, ApiBody } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
 
 @ApiTags('auth')
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
 
+  @ApiBody({ type: LoginDto })
   @Post('login')
   @UseGuards(AuthGuard('local'))
   async login(@Req() req): Promise<{ accessToken: string }> {
