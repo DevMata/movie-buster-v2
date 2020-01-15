d384f077d97666ab693c456e2f2514d0a4de8c79
Antonio Mata <hildeamp@gmail.com>
2020-01-14 21:38:45 -0600

    :recycle: refactor return value of seedRoles

--- src/roles/repositories/roles.repository.ts
+++ src/roles/repositories/roles.repository.ts
@@ -3,13 +3,13 @@ import { Role } from '../entities/role.entity';
 
 @EntityRepository(Role)
 export class RoleRepository extends Repository<Role> {
-  async seedRoles(roles: string[]): Promise<void> {
+  async seedRoles(roles: string[]): Promise<Role[]> {
     const savedRoles = (await this.find({ name: In(roles) })).map(
       role => role.name,
     );
 
     if (!roles.every(role => savedRoles.includes(role))) {
-      this.save(roles.map(role => ({ name: role })));
+      return this.save(roles.map(role => ({ name: role })));
     }
   }
 
