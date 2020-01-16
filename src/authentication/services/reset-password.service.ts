e9306ccc5d0f44ee868a9ab3b85fc3c7a1388feb
Antonio Mata <hildeamp@gmail.com>
2020-01-15 18:26:57 -0600

    :sparkles: Add logic for set new password and validate reset token

--- src/authentication/services/reset-password.service.ts
+++ src/authentication/services/reset-password.service.ts
@@ -1,12 +1,17 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { UserRepository } from '../../users/repositories/user.repository';
 import { EmailService } from '../../email/email.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AccessToken } from '../entities/token.entity';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
+import { HashHelper } from 'src/users/services/hash.helper';
 
 @Injectable()
 export class ResetPasswordService {
@@ -16,7 +21,11 @@ export class ResetPasswordService {
     private readonly emailService: EmailService,
     @InjectRepository(AccessToken)
     private readonly accessTokenRepository: Repository<AccessToken>,
-  ) {}
+    private readonly hashHelper: HashHelper,
+    private secret: string,
+  ) {
+    secret = this.configService.get('JWT_SECRET_RESET_PASSWORD');
+  }
 
   async sendResetEmail(email: string): Promise<void> {
     const user = await this.userRepository.findUserByEmail(email);
@@ -24,14 +33,28 @@ export class ResetPasswordService {
       throw new NotFoundException('user not found');
     }
 
-    const secret = this.configService.get('JWT_SECRET_RESET_PASSWORD');
     const jti = randomStringGenerator();
     const payload = { sub: user.userId, email: user.email };
 
     await this.accessTokenRepository.save({ userId: user.userId, jti });
 
-    const token = sign(payload, secret, { jwtid: jti, expiresIn: '6h' });
+    const token = sign(payload, this.secret, { jwtid: jti, expiresIn: '6h' });
 
     await this.emailService.sendResetPasswordEmail(user.email, token);
+  }
+
+  async setPassword(token: string, password: string): Promise<UpdateResult> {
+    const resetToken = verify(token, this.secret);
+
+    const { jti, sub } = resetToken as { sub: string; jti: string };
+
+    const validToken = await this.accessTokenRepository.findOne({ jti });
+    if (!validToken) {
+      throw new UnprocessableEntityException('invalid token');
+    }
+
+    return this.userRepository.update(sub, {
+      password: this.hashHelper.hash(password),
+    });
   }
 }
