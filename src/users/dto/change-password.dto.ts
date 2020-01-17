d6288b0976358380dea0db0b68ce42f15ea19e58
Antonio Mata <hildeamp@gmail.com>
2020-01-17 03:42:26 -0600

    :recycle: Decorate with ApiProperty

--- src/users/dto/change-password.dto.ts
+++ src/users/dto/change-password.dto.ts
@@ -1,6 +1,8 @@
 import { IsString, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ChangePasswordDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
