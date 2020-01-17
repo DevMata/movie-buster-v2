d37fbbfeb0567baefe642e56f8bd2852b75d3c74
Antonio Mata <hildeamp@gmail.com>
2020-01-17 01:27:36 -0600

    :white_check_mark: Add tests for validateSubOrder

--- src/order-details/order-details.service.spec.ts
+++ src/order-details/order-details.service.spec.ts
@@ -6,6 +6,10 @@ import { MovieRepository } from '../movies/repositories/movie.repository';
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { OrderDetails } from './entities/order-detail.entity';
+import {
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 
 describe('OrderDetailsService', () => {
   let service: OrderDetailsService;
@@ -13,7 +17,10 @@ describe('OrderDetailsService', () => {
   let movieRepo: MovieRepository;
 
   const mockOrderDetailsRepo = () => ({});
-  const mockMovieRepo = () => ({});
+
+  const mockMovieRepo = () => ({
+    findOne: jest.fn(),
+  });
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -36,5 +43,31 @@ describe('OrderDetailsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('validateSubOrder', () => {
+    it('should throw a not found exception when the movie is not found', () => {
+      (movieRepo.findOne as jest.Mock).mockResolvedValue(null);
+
+      expect(
+        service.validateSubOrder({ movieId: 'movieId', quantity: 1 }),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw an unprocessable entity exception when the movie stock is lesser than required quantity', () => {
+      (movieRepo.findOne as jest.Mock).mockResolvedValue({ stock: 5 });
+
+      expect(
+        service.validateSubOrder({ movieId: 'movieId', quantity: 10 }),
+      ).rejects.toThrow(UnprocessableEntityException);
+    });
+
+    it('should return the movie with updated stock', async () => {
+      (movieRepo.findOne as jest.Mock).mockResolvedValue({ stock: 5 });
+
+      expect(
+        await service.validateSubOrder({ movieId: 'movieId', quantity: 1 }),
+      ).toStrictEqual({ stock: 4 });
+    });
   });
 });
