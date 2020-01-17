76ce71f453a38e0af4d42a69341e3786b7171544
Antonio Mata <hildeamp@gmail.com>
2020-01-17 16:05:22 -0600

    :white_check_mark: Add validate tests

--- src/authentication/strategies/local.strategy.spec.ts
+++ src/authentication/strategies/local.strategy.spec.ts
@@ -1,14 +1,16 @@
+/*eslint-disable*/
+
 import { LocalStrategy } from './local.strategy';
 import { AuthenticationService } from '../services/authentication.service';
 import { Test } from '@nestjs/testing';
-
-/*eslint-disable*/
 
 describe('LocalStrategy', () => {
   let strategy: LocalStrategy;
   let authenticationService: AuthenticationService;
 
-  const mockAuthenticationService = () => ({});
+  const mockAuthenticationService = () => ({
+    validateUser: jest.fn(),
+  });
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -29,5 +31,19 @@ describe('LocalStrategy', () => {
 
   it('should be defined', () => {
     expect(strategy).toBeDefined();
+  });
+
+  describe('validate', () => {
+    it('should return authenticated user', async () => {
+      (authenticationService.validateUser as jest.Mock).mockResolvedValue(
+        'user',
+      );
+
+      expect(await strategy.validate('username', 'password')).toBe('user');
+      expect(authenticationService.validateUser).toHaveBeenCalledWith(
+        'username',
+        'password',
+      );
+    });
   });
 });
