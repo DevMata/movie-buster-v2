950cd21adcbe39aae3874892085fd1c96fde7c95
Antonio Mata <hildeamp@gmail.com>
2020-01-16 21:58:39 -0600

    :white_check_mark: Refactor OrderService test file

--- src/orders/orders.service.spec.ts
+++ src/orders/orders.service.spec.ts
@@ -1,15 +1,56 @@
+/*eslint-disable*/
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
+import { Repository, getRepository, getTreeRepository } from 'typeorm';
+import { UserRepository } from '../users/repositories/user.repository';
+import { EmailService } from '../email/email.service';
+import { OrderDetailsService } from '../order-details/order-details.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
 
 describe('OrdersService', () => {
   let service: OrdersService;
+  let orderRepo: Repository<any>;
+  let userRepo: UserRepository;
+  let orderDetailService: OrderDetailsService;
+  let emailService: EmailService;
+
+  const mockOrderRepo = () => ({});
+  const mockUserRepo = () => ({
+    findOne: jest.fn(),
+  });
+  const mockOrderDetailService = () => ({});
+  const mockEmailService = () => ({});
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [
+        OrdersService,
+        {
+          provide: getRepositoryToken(Order),
+          useFactory: mockOrderRepo,
+        },
+        {
+          provide: UserRepository,
+          useFactory: mockUserRepo,
+        },
+        {
+          provide: OrderDetailsService,
+          useFactory: mockOrderDetailService,
+        },
+        {
+          provide: EmailService,
+          useFactory: mockEmailService,
+        },
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
+    orderRepo = module.get<Repository<any>>(getRepositoryToken(Order));
+    userRepo = module.get<UserRepository>(UserRepository);
+    orderDetailService = module.get<OrderDetailsService>(OrderDetailsService);
+    emailService = module.get<EmailService>(EmailService);
   });
 
   it('should be defined', () => {
